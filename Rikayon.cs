e2e17f1e07e4dc2ee47695e5ea7b78b57eca225a
Michelle <michelle.chen.22@dartmouth.edu>
2021-01-26 02:41:58 -0800

    Add files via upload

---
+++ Rikayon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Rikayon : MonoBehaviour {
+	
+	const float locomotionAnimationSmoothTime = .1f;
+	
+	public Animator animator;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float speedPercent = agent.velocity.magnitude / agent.speed;
+		animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+
+    }
+}
